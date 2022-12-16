6912aa5001d102972e92a2ba9a0284e4434b8a06
Arland Michelena Villegas <arlandmichelenav@gmail.com>
2022-12-16 09:02:45 -0300

    starting form feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EntrevistasComponent } from './componentes/entrevistas/entrevistas.component';
+import { PreguntasFormComponent } from './componentes/preguntas/preguntas-form.component';
 import { PreguntasComponent } from './componentes/preguntas/preguntas.component';
 
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'entrevistas'},
   {path:'entrevistas', component: EntrevistasComponent},
   {path:'preguntas', component: PreguntasComponent},
+  {path:'preguntas/form', component: PreguntasFormComponent},
   {path:'**', pathMatch: 'full', redirectTo: 'preguntas'},
 ];
-
+//preguntas/form
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { AppComponent } from './app.component';
 import { EntrevistasComponent } from './componentes/entrevistas/entrevistas.component';
 import { PreguntasComponent } from './componentes/preguntas/preguntas.component';
 import { LayoutModule } from './layout/layout.module';
-import { PreguntasFormComponent } from './componentes/preguntas/preguntas-form/preguntas-form.component';
+import { PreguntasFormComponent } from './componentes/preguntas/preguntas-form.component';
+
 
 @NgModule({
   declarations: [
